19b3a980b93c85f354cbfba5dff621de0ef3973d
Darian | Praxis <hello@vulpinecat.com>
2019-03-22 13:30:26 +0100

    add camera panning and tilting

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -7,26 +7,72 @@ public class camera : MonoBehaviour
     public GameObject player;
     private Player playerScript;
 
+
+    public Vector3 goalOffset;
+    public Vector3 goalAngle;
+
     private Vector3 offset;
     private Vector3 lastPosition;
+
+    public float rotationSpeed = 2f;
+    public float cameraSpeed = 0.2f;
+
+    private Vector3 upOffset = new Vector3(0, 0, 6);
+    private Vector3 upAngle = new Vector3(-20, 0, 0);
+
+    private Vector3 downOffset = new Vector3(0, -1, 2);
+    private Vector3 downAngle = new Vector3(10, 0, 0);
+
+    private Vector3 rightOffset = new Vector3(-2, 0, 0);
+    private Vector3 rightAngle = new Vector3(0, 10, 0);
+
+    private Vector3 leftOffset = new Vector3(2, 0, 0);
+    private Vector3 leftAngle = new Vector3(0, 350, 0);
+
+    public Vector3 cameraVelocity = Vector3.zero;
+    public Vector3 offsetVelocity = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
+
         playerScript = player.GetComponent<Player>();
         offset = transform.position - player.transform.position;
         lastPosition = player.transform.position;
+
+        goalOffset = offset;
+        goalAngle = transform.eulerAngles;
+    }
+
+    void Update() {
+        UpdateOffsetAndAngle("up", upOffset, upAngle);
+        UpdateOffsetAndAngle("down", downOffset, downAngle);
+        UpdateOffsetAndAngle("right", rightOffset, rightAngle);
+        UpdateOffsetAndAngle("left", leftOffset, leftAngle);
+    }
+
+    void UpdateOffsetAndAngle(string key, Vector3 keyOffset, Vector3 keyAngle) {
+        if (Input.GetKeyDown(key)) {
+            goalOffset += keyOffset;
+            goalAngle += keyAngle;
+        }
+
+        if (Input.GetKeyUp(key)) {
+            goalOffset -= keyOffset;
+            goalAngle -= keyAngle;
+        }
     }
 
     void LateUpdate()
     {
-        Vector3 movementDelta = lastPosition - player.transform.position;
-        Vector3 goalPos = player.transform.position + offset;
-        Vector3 fromPos = transform.position;
-        Vector3 moveTo = (goalPos - fromPos) * playerScript.acceleration;
-        if(moveTo != Vector3.zero) {
-            transform.position = transform.position + moveTo;
+        if(transform.eulerAngles != goalAngle) {
+         transform.rotation =  Quaternion.Slerp(transform.rotation, Quaternion.Euler(goalAngle), rotationSpeed *  Time.deltaTime);
         }
 
-        lastPosition = player.transform.position;
+        if (offset != goalOffset) {
+            offset = Vector3.SmoothDamp(offset, goalOffset, ref offsetVelocity, cameraSpeed);
+        }
+
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + offset, ref cameraVelocity, cameraSpeed);
     }
 }
